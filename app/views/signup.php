b6bf6ff5edc3573039105d38f9cccc1185ed5ec9
florian <bessonflorian42@yahoo.fr>
2025-09-09 23:01:52 +0200

    micro mvc

---
+++ app/views/signup.php
@@ -0,0 +1,7 @@
+<h1>Créer un compte</h1>
+<form method="post">
+  <input type="text" name="username" placeholder="Nom d'utilisateur">
+  <input type="password" name="password" placeholder="Mot de passe">
+  <button type="submit">S'inscrire</button>
+</form>
+
--- public/index.php
+++ public/index.php
@@ -1,9 +1,14 @@
 <?php
-declare(strict_types=1);
-http_response_code(200);
-?><!doctype html>
-<html><head><meta charset="utf-8"><title>Camagru</title></head>
-<body>
-  <h1>camagru up</h1>
-  <p>hello world.</p>
-</body></html>
+declare(strict_types=1); //force le typage pas de declaration bizarre
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH); //parse l'url sans query string
+if (!$path) $path = '/';
+
+//tableau cle=>valeur micro mvc
+$routes = [
+  '/' => fn() => require __DIR__ . '/../app/views/home.php',
+  '/signup' => fn() => require __DIR__ . '/../app/views/signup.php',
+  '/login' => fn() => require __DIR__ . '/../app/views/login.php',
+];
+
+if (isset($routes[$path])) { $routes[$path](); exit; }
+http_response_code(404); echo "Not found";
