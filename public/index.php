b4c38eca4bee1ff4edf5f1448a16dfc95ad903e6
florian <bessonflorian42@yahoo.fr>
2025-09-09 22:05:06 +0200

    fsd

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+http_response_code(200);
+?><!doctype html>
+<html><head><meta charset="utf-8"><title>Camagru</title></head>
+<body>
+  <h1>camagru up</h1>
+  <p>hello world.</p>
+</body></html>
